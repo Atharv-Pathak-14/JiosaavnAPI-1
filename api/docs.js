2dbb5a51e44c9bbb339f11c2f1e42f25b3d24b3e
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2023-02-02 14:41:13 +0530

    Delete api directory

--- api/docs.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const swaggerUi = require("swagger-ui-express");
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("../docs/swagger"))); // swagger-ui-express
-
-module.exports = app;
